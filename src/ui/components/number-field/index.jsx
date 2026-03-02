85a12c09e92ca07827efc5c1fb4b84d460afe032
Asror <khberv.dev@gmail.com>
2026-03-02 20:27:18 +0500

    fix: convert number

--- src/ui/components/number-field/index.jsx
+++ src/ui/components/number-field/index.jsx
@@ -1,11 +1,11 @@
 import { TextField } from "@mui/material";
-import { formatNumber } from "@/utils/formatter.js";
+import { extractNumbers, formatNumber } from "@/utils/formatter.js";
 
 function NumberField({ variant = 'standard', label, value, onChange, ...props }) {
     const handleChange = (e) => {
         const val = formatNumber(e.target.value)
 
-        onChange(val)
+        onChange(extractNumbers(val))
     }
 
     return (
