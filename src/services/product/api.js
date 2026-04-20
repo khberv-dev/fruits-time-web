021f21f9358126c6000703ece4515b76058bfc22
Asror <khberv.dev@gmail.com>
2026-04-20 10:47:55 +0500

    update: product API

--- src/services/product/api.js
+++ src/services/product/api.js
@@ -1,26 +1,26 @@
 import {api} from "@/services/api.js";
 
 export async function getAllProducts(catalogId, locale) {
-    const res = await api.get(`${catalogId}/product/all`, {params: {locale}})
+    const res = await api.get(`catalog/${catalogId}/product/all`, {params: {locale}})
     return res.data
 }
 
 export async function getProduct(catalogId, productId, locale) {
-    const res = await api.get(`${catalogId}/product/all`, {params: {locale}})
+    const res = await api.get(`catalog/${catalogId}/product/all`, {params: {locale}})
     return res.data.find((p) => String(p.id) === String(productId)) ?? null
 }
 
 export async function createProduct(catalogId, data, locale) {
-    const res = await api.post(`${catalogId}/product`, data, {params: {locale}})
+    const res = await api.post(`catalog/${catalogId}/product`, data, {params: {locale}})
     return res.data
 }
 
 export async function updateProduct(catalogId, productId, data, locale) {
-    const res = await api.put(`${catalogId}/product/${productId}`, data, {params: {locale}})
+    const res = await api.put(`catalog/${catalogId}/product/${productId}`, data, {params: {locale}})
     return res.data
 }
 
 export async function deleteProduct(catalogId, productId) {
-    const res = await api.delete(`${catalogId}/product/${productId}`)
+    const res = await api.delete(`catalog/${catalogId}/product/${productId}`)
     return res.data
 }
